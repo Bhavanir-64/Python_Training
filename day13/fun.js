10d9e8dc8a067175047ce9dce77144ec9409f569
First github <name1@gmail.com>
2025-06-10 20:26:55 +0530

    push

---
+++ day13/fun.js
@@ -0,0 +1,35 @@
+// function greet(name) {
+//   console.log("hii", name);
+// }
+// greet("Ram");
+
+// const a = () => console.log("hii");
+// a();
+
+// a = [1, 2, 3, 4];
+// a.forEach((ele) => {
+//   console.log(ele);
+// });
+
+a = [1, 2, 3, 4, 5, 6];
+b = a.filter((i) => i % 2 == 0);
+console.log(b);
+
+// function func(name, c) {
+//   console.log("Hai", name);
+//   c();
+// }
+
+// function say() {
+//   console.log("Bye");
+// }
+// func("niharika", say);
+
+// function operate(a, b, opearation) {
+//   return opearation(a, b);
+// }
+// const add = (x, y) => x + y;
+// console.log(operate(4, 5, add));
+
+// arr = [2, 3, 4, 5, 6];
+// console.log(arr.map((i) => i * i));
